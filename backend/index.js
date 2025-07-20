b66b4550275f0e748db9ada39a05506423b61b9b
lukir23 <lukarepen@gmail.com>
2025-07-20 14:44:43 +0200

    vzpostavljen express streznik in povezava z bazo

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+});
+
+
+db.connect(err => {
+  if (err) {
+    console.error('Povezava z bazo ni uspela:', err);
+  } else {
+    console.log('Povezano z MySQL bazo!');
+  }
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Strežnik deluje!');
+});
+
+
+app.listen(3001, () => {
+  console.log('Strežnik teče na http://localhost:3001');
+});
